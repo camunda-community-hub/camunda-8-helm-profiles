0cb89442a5def31b4c8d99827c42f614ce027305
Dave Paroulek <david.paroulek@camunda.com>
2022-08-03 14:54:21 -0400

    Testing and fixing bugs for for ingress-azure

--- include/ingress-azure.mk
+++ include/ingress-azure.mk
@@ -1,19 +1,21 @@
 
 .PHONY: ingress
-ingress: namespace ingress-ip-from-commandline ingress-ip ingress-azure
+ingress: namespace ingress-ip-from-commandline ingress-azure
 
 .PHONY: ingress-azure
 ingress-azure:
 	kubectl apply -f ingress-azure.yaml
 
+.PHONY: ingress-ip-from-service
+ingress-ip-from-service:
+	IP=$$(kubectl get service -w ingress-nginx-controller -o 'go-template={{with .status.loadBalancer.ingress}}{{range .}}{{.ip}}{{"\n"}}{{end}}{{.err}}{{end}}' -n ingress-nginx 2>/dev/null | head -n1) ; \
+	sed -Ei '' "s/([0-9]{1,3}\.){3}[0-9]{1,3}/$$IP/g" camunda-values.yaml ; \
+	echo "Ingress controller ready at: http://$$IP.nip.io"
+
 .PHONY: ingress-ip-from-commandline
 ingress-ip-from-commandline:
-	@echo "Enter IP Address of AWS Load Balancer: "; \
-	read IP;
-	@echo "IP Address: $$IP"
-
-.PHONY: ingress-ip
-ingress-ip:
+	@echo "Enter Load Balancer IP Address: " ; \
+	read IP; \
 	sed -Ei '' "s/([0-9]{1,3}\.){3}[0-9]{1,3}/$$IP/g" camunda-values.yaml ; \
 	sed -Ei '' "s/([0-9]{1,3}\.){3}[0-9]{1,3}/$$IP/g" ingress-azure.yaml ; \
 	echo "Ingress controller ready at: http://$$IP.nip.io"
