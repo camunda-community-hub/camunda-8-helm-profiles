cf55ef2a8581d736522101dc518ddb33c2ff0bf7
Dave Paroulek <david.paroulek@camunda.com>
2022-09-01 20:35:14 -0400

    bugfix: use version of sed that works on both mac and linux

--- include/ingress-azure.mk
+++ include/ingress-azure.mk
@@ -9,15 +9,15 @@ ingress-azure:
 .PHONY: ingress-ip-from-service
 ingress-ip-from-service:
 	IP=$$(kubectl get service -w ingress-nginx-controller -o 'go-template={{with .status.loadBalancer.ingress}}{{range .}}{{.ip}}{{"\n"}}{{end}}{{.err}}{{end}}' -n ingress-nginx 2>/dev/null | head -n1) ; \
-	sed -Ei '' "s/([0-9]{1,3}\.){3}[0-9]{1,3}/$$IP/g" camunda-values.yaml ; \
+	sed -i.bak "s/([0-9]{1,3}\.){3}[0-9]{1,3}/$$IP/g" camunda-values.yaml ; \
 	echo "Ingress controller ready at: http://$$IP.nip.io"
 
 .PHONY: ingress-ip-from-commandline
 ingress-ip-from-commandline:
 	@echo "Enter Load Balancer IP Address: " ; \
 	read IP; \
-	sed -Ei '' "s/([0-9]{1,3}\.){3}[0-9]{1,3}/$$IP/g" camunda-values.yaml ; \
-	sed -Ei '' "s/([0-9]{1,3}\.){3}[0-9]{1,3}/$$IP/g" ingress-azure.yaml ; \
+	sed -i.bak "s/([0-9]{1,3}\.){3}[0-9]{1,3}/$$IP/g" camunda-values.yaml ; \
+	sed -i.bak "s/([0-9]{1,3}\.){3}[0-9]{1,3}/$$IP/g" ingress-azure.yaml ; \
 	echo "Ingress controller ready at: http://$$IP.nip.io"
 
 .PHONY: clean-ingress
@@ -26,5 +26,5 @@ clean-ingress: clean-ingress-ip
 
 .PHONY: clean-ingress-ip
 clean-ingress-ip:
-	sed -Ei '' "s/([0-9]{1,3}\.){3}[0-9]{1,3}/127.0.0.1/g" camunda-values.yaml ; \
-	sed -Ei '' "s/([0-9]{1,3}\.){3}[0-9]{1,3}/127.0.0.1/g" ingress-azure.yaml ; \
+	sed -i.bak "s/([0-9]{1,3}\.){3}[0-9]{1,3}/127.0.0.1/g" camunda-values.yaml ; \
+	sed -i.bak "s/([0-9]{1,3}\.){3}[0-9]{1,3}/127.0.0.1/g" ingress-azure.yaml ; \
