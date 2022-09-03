d955897f6882d10454ea03bbbc761fb4712fc692
Dave Paroulek <david.paroulek@camunda.com>
2022-09-02 21:58:33 -0400

    clean up ingress-azure targets

--- include/ingress-azure.mk
+++ include/ingress-azure.mk
@@ -1,13 +1,9 @@
-
-.PHONY: ingress
-ingress: namespace ingress-azure
-
 .PHONY: ingress-azure
-ingress-azure:
+ingress-azure: namespace
 	$(eval IP := $(shell az network public-ip show -g $(nodeResourceGroup) -n $(gatewayName)-appgwpip -o json --query ipAddress | xargs))
 	echo "Creating ingress controller at: http://$(IP).nip.io" ;
 	cat ingress-azure.yaml | sed -E "s/([0-9]{1,3}\.){3}[0-9]{1,3}/$(IP)/g" | kubectl apply -f -
 
-.PHONY: clean-ingress
-clean-ingress: clean-ingress-ip
+.PHONY: clean-ingress-azure
+clean-ingress-azure:
 	kubectl delete ingress ingress-azure -n camunda
