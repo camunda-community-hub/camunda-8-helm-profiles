6f43166e0d1e65bc00b58ad551ecec8fd279f1f0
rob2universe <robert.emsbach@gmail.com>
2022-10-14 01:14:29 +0800

    use kubectl wait instead of while loop

--- include/camunda.mk
+++ include/camunda.mk
@@ -24,7 +24,7 @@ keycloak-password:
 
 .PHONY: config-keycloak
 config-keycloak: keycloak-password
-	@while [ $$(kubectl get pods $(release)-keycloak-0 -o 'jsonpath={..status.conditions[?(@.type=="Ready")].status}') != "True" ]; do echo "Waiting for Keycloak pod..." && sleep 3; done
+	kubectl wait --for=condition=Ready pod -l app.kubernetes.io/component=keycloak --timeout=300s
 	kubectl -n $(namespace) exec -it $(release)-keycloak-0 -- /opt/bitnami/keycloak/bin/kcadm.sh update realms/master -s sslRequired=NONE --server http://localhost:8080/auth --realm master --user admin --password $(kcPassword)
 	kubectl -n $(namespace) exec -it $(release)-keycloak-0 -- /opt/bitnami/keycloak/bin/kcadm.sh update realms/camunda-platform -s sslRequired=NONE --server http://localhost:8080/auth --realm master --user admin --password $(kcPassword)
 
