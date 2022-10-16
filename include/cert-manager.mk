0096b40922b5a3353f5008d8f36cb68e2d868d3d
rob2universe <robert.emsbach@gmail.com>
2022-10-16 10:21:26 +0800

    cert-manager cleanup

--- include/cert-manager.mk
+++ include/cert-manager.mk
@@ -1,4 +1,6 @@
 # https://cert-manager.io/docs/tutorials/acme/nginx-ingress/
+prodserver ?= https:\\/\\/acme-v02.api.letsencrypt.org/directory
+
 .PHONY: cert-manager
 cert-manager: 
 	kubectl create namespace cert-manager
@@ -10,7 +12,15 @@ cert-manager:
   	--version v1.9.1 \
 	--set installCRDs=true
 
-.PHONY: issuers
-issuers:
-	cat letsencypt-staging.yaml | sed -E "s/someone@somewhere.io/$(certEmail)/g" | kubectl create -n cert-manager -f -
-	cat letsencypt-prod.yaml | sed -E "s/someone@somewhere.io/$(certEmail)/g" | kubectl create -n cert-manager -f -
+.PHONY: letsencypt-staging
+letsencypt-staging:
+	cat ../include/letsencypt.yaml | sed -E "s/someone@somewhere.io/$(certEmail)/g" | kubectl create -n cert-manager -f -
+	
+.PHONY: letsencypt-prod
+letsencypt-prod:
+	cat ../include/letsencypt.yaml | sed -E "s/someone@somewhere.io/$(certEmail)/g" | sed -E "s/acme-staging-v02/acme-v02/g" | kubectl apply -n cert-manager -f -
+
+.PHONY: letsencypt-prod-patch
+letsencypt-prod-patch:
+	kubectl patch ClusterIssuer letsencrypt --type json -p '[{"op": "replace", "path": "/spec/acme/sever", "value":"$(prodserver)"}]'
+	kubectl describe ClusterIssuer letsencrypt | grep letsencrypt.org
