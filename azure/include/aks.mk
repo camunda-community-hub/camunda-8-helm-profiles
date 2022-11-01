0505fec52cbfc88501f2a6ef6fca8f242f8459da
Dave Paroulek <david.paroulek@camunda.com>
2022-11-01 10:28:49 -0400

    able to create azure aks cluster with or without app gateway

--- azure/include/aks.mk
+++ azure/include/aks.mk
@@ -1,5 +1,5 @@
-.PHONY: kube
-kube:
+.PHONY: kube-nginx
+kube-nginx:
 	az group create --name $(resourceGroup) --location $(region)
 	az aks create \
       --resource-group $(resourceGroup) \
@@ -11,6 +11,28 @@ kube:
       --min-count $(minSize) \
       --max-count $(maxSize) \
       --enable-managed-identity \
+      --generate-ssh-keys
+	kubectl config unset clusters.$(clusterName)
+	kubectl config unset users.clusterUser_$(resourceGroup)_$(clusterName)
+	az aks get-credentials --resource-group $(resourceGroup) --name $(clusterName)
+	kubectl apply -f $(root)azure/include/ssd-storageclass.yaml
+
+.PHONY: kube-agic
+kube-agic:
+	az group create --name $(resourceGroup) --location $(region)
+	az aks create \
+      --resource-group $(resourceGroup) \
+      --name $(clusterName) \
+      --node-vm-size $(machineType) \
+      --node-count 1 \
+      --vm-set-type VirtualMachineScaleSets \
+      --enable-cluster-autoscaler \
+      --min-count $(minSize) \
+      --max-count $(maxSize) \
+      --enable-managed-identity \
+      -a ingress-appgw \
+      --appgw-name $(gatewayName) \
+      --appgw-subnet-cidr "10.225.0.0/16" \
       --generate-ssh-keys
 	kubectl config unset clusters.$(clusterName)
 	kubectl config unset users.clusterUser_$(resourceGroup)_$(clusterName)
