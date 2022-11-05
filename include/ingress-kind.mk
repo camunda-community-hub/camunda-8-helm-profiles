6dbf110681a602e9b3c31e6c65f472d8f890848a
paul.lungu <paul.lungu@camunda.com>
2022-11-04 19:35:36 -0600

    create creation scripts and ingress mods for gateway tls

--- include/ingress-kind.mk
+++ include/ingress-kind.mk
@@ -3,10 +3,11 @@ ingress-kind: ingress-nginx-kind
 
 .PHONY: ingress-nginx-kind
 ingress-nginx-kind:
-	-kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/main/deploy/static/provider/kind/deploy.yaml
+	kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/main/deploy/static/provider/kind/deploy.yaml
+	kubectl delete -A ValidatingWebhookConfiguration ingress-nginx-admission
 
 .PHONY: clean-ingress
 clean-ingress:
-	-helm --namespace ingress-nginx uninstall ingress-nginx
-	-kubectl delete -n ingress-nginx pvc -l app.kubernetes.io/instance=ingress-nginx
-	-kubectl delete namespace ingress-nginx
+	helm --namespace ingress-nginx uninstall ingress-nginx
+	kubectl delete -n ingress-nginx pvc -l app.kubernetes.io/instance=ingress-nginx
+	kubectl delete namespace ingress-nginx
