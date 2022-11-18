056801b03af6af8d6bda4afe471c9fcc7e4b0e83
Christophe <christophe.dame@camunda.com>
2022-11-18 11:56:28 +0100

    update PR with latest feedbacks

--- benchmark/benchmark.mk
+++ benchmark/benchmark.mk
@@ -1,18 +1,18 @@
 
 .PHONY: deploy-models
 deploy-models:
-	kubectl create configmap models --from-file=BenchmarkProcess.bpmn     -n $(namespace)
-	kubectl apply -f zbctl-deploy-job.yaml                                -n $(namespace)
+	kubectl create configmap models --from-file=$(root)/benchmark/BenchmarkProcess.bpmn     -n $(namespace)
+	kubectl apply -f $(root)/benchmark/zbctl-deploy-job.yaml                                -n $(namespace)
 	kubectl wait --for=condition=complete job/zbctl-deploy --timeout=300s -n $(namespace)
 	kubectl delete configmap models                                       -n $(namespace)
-	kubectl delete -f zbctl-deploy-job.yaml                               -n $(namespace)
+	kubectl delete -f $(root)/benchmark/zbctl-deploy-job.yaml                               -n $(namespace)
 
 .PHONY: benchmark
 benchmark: namespace
-	kubectl create configmap payload --from-file=payload.json -n $(namespace)
-	kubectl apply -f benchmark.yaml                           -n $(namespace)
+	kubectl create configmap payload --from-file=$(root)/benchmark/payload.json -n $(namespace)
+	kubectl apply -f $(root)/benchmark/benchmark.yaml         -n $(namespace)
 
 .PHONY: clean-benchmark
 clean-benchmark:
-	-kubectl delete -f benchmark.yaml -n $(namespace)
+	-kubectl delete -f $(root)/benchmark/benchmark.yaml -n $(namespace)
 	-kubectl delete configmap payload -n $(namespace)
