0c1cacb0863df803ddaf6f50d6e60144d7e951ab
Dave Paroulek <david.paroulek@camunda.com>
2023-03-22 07:51:05 -0400

    Azure - working on secure-zeebe-ingress

--- azure/include/aks.mk
+++ azure/include/aks.mk
@@ -55,24 +55,26 @@ azure-dns-zone:
 	az network dns zone create -g $(resourceGroup) -n $(baseDomainName)
 
 .PHONY: azure-dns-record
-azure-dns-record:
+azure-dns-record: ingress-ip-from-service
 	az network dns record-set a add-record -g $(resourceGroup) -z $(baseDomainName) -n $(subDomainName) -a $(IP)
+
+.PHONY: azure-dns
+azure-dns: azure-dns-zone azure-dns-record
 
 .PHONY: azure-nginx-dns-tls
 azure-nginx-dns-tls:
 	helm repo add ingress-nginx https://kubernetes.github.io/ingress-nginx
 	helm repo update ingress-nginx
 	helm search repo ingress-nginx
-	ifdef $(dnsLabel)
-	helm install ingress-nginx ingress-nginx/ingress-nginx --namespace ingress-nginx --create-namespace --wait \
-	--set controller.service.annotations."service\.beta\.kubernetes\.io/azure-dns-label-name"=$(dnsLabel) \
-	--set controller.service.annotations."nginx\.ingress.kubernetes.io/ssl-redirect"="true" \
-	--set controller.service.annotations."cert-manager.io/cluster-issuer"="letsencrypt" \
-	--set controller.config.error-log-level="debug"
-	else
-	helm install ingress-nginx ingress-nginx/ingress-nginx --namespace ingress-nginx --create-namespace --wait \
-	--set controller.service.annotations."service\.beta\.kubernetes\.io/azure-dns-label-name"=$(dnsLabel) \
-	--set controller.service.annotations."nginx\.ingress.kubernetes.io/ssl-redirect"="true" \
-	--set controller.service.annotations."cert-manager.io/cluster-issuer"="letsencrypt" \
-	--set controller.config.error-log-level="debug"
-	endif
+	if [ -n "$(dnsLabel)" ]; then \
+	  helm install ingress-nginx ingress-nginx/ingress-nginx --namespace ingress-nginx --create-namespace --wait \
+	  --set controller.service.annotations."service\.beta\.kubernetes\.io/azure-dns-label-name"=$(dnsLabel) \
+	  --set controller.service.annotations."nginx\.ingress.kubernetes.io/ssl-redirect"="true" \
+	  --set controller.service.annotations."cert-manager.io/cluster-issuer"="letsencrypt" \
+	  --set controller.config.error-log-level="debug"; \
+	else \
+	  helm install ingress-nginx ingress-nginx/ingress-nginx --namespace ingress-nginx --create-namespace --wait \
+	  --set controller.service.annotations."nginx\.ingress.kubernetes.io/ssl-redirect"="true" \
+	  --set controller.service.annotations."cert-manager.io/cluster-issuer"="letsencrypt" \
+	  --set controller.config.error-log-level="debug"; \
+	fi
