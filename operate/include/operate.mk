e9ec9a2c8651a92777b0ee7ad954feb3aefed1bd
Dave Paroulek <david.paroulek@camunda.com>
2023-07-18 15:48:16 -0400

    (almost!) able to scale operate. 1 works but 2 replicas redirect forever

--- operate/include/operate.mk
+++ operate/include/operate.mk
@@ -10,8 +10,14 @@ operate-deployment-importer-archiver.yaml: fqdn
 operate-deployment-webapp.yaml: fqdn
 	sed "s/YOUR_HOSTNAME/$(fqdn)/g;" $(root)/operate/include/deployment-webapp.tpl.yaml > ./operate-deployment-webapp.yaml;
 
+#operate-service-importer-archiver.yaml:
+#	cp $(root)/operate/include/service-importer-archiver.tpl.yaml ./operate-service-importer-archiver.yaml;
+
+operate-service-webapp.yaml:
+	cp $(root)/operate/include/service-webapp.tpl.yaml ./operate-service-webapp.yaml;
+
 .PHONY: operate-yaml-files
-operate-yaml-files: operate-configmap-importer-archiver.yaml operate-configmap-webapp.yaml operate-deployment-importer-archiver.yaml operate-deployment-webapp.yaml
+operate-yaml-files: operate-configmap-importer-archiver.yaml operate-configmap-webapp.yaml operate-deployment-importer-archiver.yaml operate-deployment-webapp.yaml operate-service-webapp.yaml #operate-service-importer-archiver.yaml
 
 .PHONY: clean-operate-yaml-files
 operate-clean-yaml-files:
@@ -19,14 +25,29 @@ operate-clean-yaml-files:
 	rm ./operate-configmap-webapp.yaml;
 	rm ./operate-deployment-importer-archiver.yaml;
 	rm ./operate-deployment-webapp.yaml;
+#	rm ./operate-service-importer-archiver.yaml;
+	rm ./operate-service-webapp.yaml;
 
 .PHONY: operate-delete
 operate-delete:
 	kubectl get deployments -l app.kubernetes.io/component=operate -o name | xargs kubectl delete
+	kubectl get service -l app.kubernetes.io/component=operate -o name | xargs kubectl delete
 
 .PHONY: operate-install
 operate-install: operate-yaml-files
-	kubectl apply -f ./configmap-importer-archiver.yaml
-	kubectl apply -f ./configmap-webapp-archiver.yaml
-	kubectl apply -f ./deployment-importer-archiver.yaml
-	kubectl apply -f ./deployment-webapp.yaml
+	kubectl apply -f ./operate-configmap-importer-archiver.yaml
+	kubectl apply -f ./operate-configmap-webapp.yaml
+	kubectl apply -f ./operate-deployment-importer-archiver.yaml
+	kubectl apply -f ./operate-deployment-webapp.yaml
+#	kubectl apply -f ./operate-service-importer-archiver.yaml
+	kubectl apply -f ./operate-service-webapp.yaml
+
+.PHONY: operate-clean
+operate-clean: operate-clean-yaml-files
+	kubectl delete -f ./operate-deployment-importer-archiver.yaml
+	kubectl delete -f ./operate-deployment-webapp.yaml
+	kubectl delete -f ./operate-configmap-importer-archiver.yaml
+	kubectl delete -f ./operate-configmap-webapp.yaml
+#	kubectl delete -f ./operate-service-importer-archiver.yaml
+	kubectl delete -f ./operate-service-webapp.yaml
+
