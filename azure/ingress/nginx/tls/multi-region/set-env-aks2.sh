09bb02d8beebdcf561b54687b321ae02ee6fbcb0
Dave Paroulek <david.paroulek@camunda.com>
2024-01-13 21:58:01 -0500

    Updated environment variables for azure multi region

--- azure/ingress/nginx/tls/multi-region/set-env-aks2.sh
+++ azure/ingress/nginx/tls/multi-region/set-env-aks2.sh
@@ -1,18 +1,18 @@
 export certEmail=david.paroulek@camunda.com
-export clusterName=dave01
-export resourceGroup=dave01-rg
+export clusterName=dave02
+export resourceGroup=dave02-rg
 
 export camundaVersion=8.4.0
 export camundaHelmVersion=9.0.1
 
-export dnsLabel=dave01
+export dnsLabel=dave02
 export baseDomainName=aks.c8sm.com
-#export baseDomainName=eastus.cloudapp.azure.com
+#export baseDomainName=canadacentral.cloudapp.azure.com
 export dnsManagedZone=aks
 
-export region=eastus
-export namespace=camunda-eastus
-export namespace2=camunda-canadacentral
+export region=canadacentral
+export namespace=camunda-canadacentral
+export namespace2=camunda-eastus
 
 export machineType=Standard_A8_v2
 export minSize=1
@@ -24,13 +24,14 @@ export camundaDockerRegistryUsername="xxx"
 export camundaDockerRegistryPassword="xxx"
 export camundaDockerRegistryEmail="xxx"
 
-export addressPrefix="10.1.0.0/16"
-export nodeSubnetPrefix="10.1.240.0/24"
-export podSubnetPrefix="10.1.241.0/24"
-export serviceCidr="10.0.1.0/24"
-export dnsServiceIp="10.0.1.10"
-export dnsLBIp="20.104.45.90"
+export addressPrefix="10.2.0.0/16"
+export nodeSubnetPrefix="10.2.240.0/24"
+export podSubnetPrefix="10.2.241.0/24"
+export serviceCidr="10.0.2.0/24"
+export dnsServiceIp="10.0.2.10"
+export dnsLBIp="52.179.112.26"
 
 export regions=2
-export regionId=0
+export regionId=1
+
 
