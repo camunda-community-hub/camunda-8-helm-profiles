555fee8155e07e417417ebcacc05608845069258
Dave Paroulek <david.paroulek@camunda.com>
2024-07-29 14:55:32 -0400

    Attempting to configure operate re-encrypt route in openshift

--- openshift/set-env-openshift.sh
+++ openshift/set-env-openshift.sh
@@ -3,7 +3,7 @@ export userName=kubeadmin
 export userSecret="xxx"
 
 export camundaVersion=8.5.4
-export camundaHelmVersion=10.2.0
+export camundaHelmVersion=10.2.1
 
 export release=camunda
 export chartValues=camunda-values-openshift.yaml
