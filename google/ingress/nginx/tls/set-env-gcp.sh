cab493b5db7513f9221769ca88b371e1c1bf5fd1
Dave Paroulek <david.paroulek@camunda.com>
2025-01-10 14:15:39 -0500

    added set-env script for gcp

---
+++ google/ingress/nginx/tls/set-env-gcp.sh
@@ -0,0 +1,24 @@
+export certEmail="xxx"
+export clusterName="dave01"
+export project="xxx"
+
+export camundaVersion=8.6.6
+export camundaHelmVersion=11.1.0
+
+export dnsLabel="dave01"
+export baseDomainName="gke.c8sm.com"
+export dnsManagedZone="c8sm"
+
+#export region="us-central1-a"
+export region="us-east4-a"
+export namespace="camunda"
+
+export machineType=n1-standard-16
+export minSize=1
+export maxSize=50
+
+export camundaDockerRegistrySecretName="camunda-docker-registry"
+export camundaDockerRegistryUrl="https://registry.camunda.cloud/"
+export camundaDockerRegistryUsername="xxx"
+export camundaDockerRegistryPassword="xxx"
+export camundaDockerRegistryEmail="xxx"
