010b063d901a5001e103d314381eaddc532945ec
Jothi <Jothikiruthika.viswanathan@camunda.com>
2025-04-23 10:44:01 +0800

    Sync with main branch

--- google/include/kubernetes-gke.mk
+++ google/include/kubernetes-gke.mk
@@ -14,10 +14,9 @@ kube-gke:
 	gcloud container clusters create $(clusterName) \
 	  --region $(region) \
 	  --num-nodes=1 \
-	  --enable-autoscaling --max-nodes=$(maxSize) --min-nodes=$(minSize) \
+	  --enable-autoscaling --total-max-nodes=$(maxSize) --total-min-nodes=$(minSize) \
 	  --enable-ip-alias \
 	  --machine-type=$(machineType) \
-	  --disk-type "pd-ssd" \
 	  --spot \
 	  --maintenance-window=4:00 \
 	  --release-channel=regular \
@@ -28,13 +27,12 @@ kube-gke:
 	gcloud container clusters get-credentials $(clusterName) --region $(region)
 
 .PHONY: node-pool # create an additional Kubernetes node pool
-node-pool: use-kube
+node-pool:
 	gcloud beta container node-pools create "pool-$(machineType)" \
 	  --project $(project) \
 	  --cluster $(clusterName) \
 	  --region $(region) \
 	  --machine-type "$(machineType)" \
-	  --disk-type "pd-ssd" \
 	  --spot \
 	  --num-nodes=0 \
 	  --enable-autoscaling --total-min-nodes "0" --total-max-nodes $(maxSize) --location-policy "ANY" \
@@ -42,15 +40,15 @@ node-pool: use-kube
 	  --enable-autoupgrade \
 	  --enable-autorepair \
 	  --max-surge-upgrade 0 --max-unavailable-upgrade 1
-#	  --node-version "1.27.3-gke.1700" \
-#	  --image-type "COS_CONTAINERD" \
-#	  --disk-size "100" \
-#	  --metadata disable-legacy-endpoints=true \
-#	  --scopes "https://www.googleapis.com/auth/devstorage.read_only","https://www.googleapis.com/auth/logging.write","https://www.googleapis.com/auth/monitoring","https://www.googleapis.com/auth/servicecontrol","https://www.googleapis.com/auth/service.management.readonly","https://www.googleapis.com/auth/trace.append" \
-# C4 requires hyperdisk: gcloud beta container --project "camunda-researchanddevelopment" node-pools create "pool-c4-standard-8" --cluster "falko-benchmark" --zone "europe-west1-b" --node-version "1.30.3-gke.1969001" --machine-type "c4-standard-8" --image-type "COS_CONTAINERD" --disk-type "hyperdisk-balanced" --disk-size "100" --metadata disable-legacy-endpoints=true --scopes "https://www.googleapis.com/auth/devstorage.read_only","https://www.googleapis.com/auth/logging.write","https://www.googleapis.com/auth/monitoring","https://www.googleapis.com/auth/servicecontrol","https://www.googleapis.com/auth/service.management.readonly","https://www.googleapis.com/auth/trace.append" --spot --num-nodes "1" --enable-autoscaling --min-nodes "0" --max-nodes "256" --location-policy "ANY" --enable-autoupgrade --enable-autorepair --max-surge-upgrade 1 --max-unavailable-upgrade 0
+# C4 requires hyperdisk. Thus letting GKE pick the disk type.
+#	  --disk-type "pd-ssd" # this should only be needed for working with local storage, i.e. without PVC \
+#	  --disk-size "100" # 100GiB is the default.\
+# TODO for faster benchmark startup `--num-nodes` could be iniziallized by Zeebe Tuner with the expected number of Zeebe Brokers, Gateways, and ES nodes
 
-# original command suggested by Web Console:
-# gcloud beta container --project "camunda-researchanddevelopment" node-pools create "pool-c3-standard-8" --cluster "falko-benchmark-16" --zone "europe-west1-b" --node-version "1.27.3-gke.1700" --machine-type "c3-standard-8" --image-type "COS_CONTAINERD" --disk-type "pd-ssd" --disk-size "100" --metadata disable-legacy-endpoints=true --scopes "https://www.googleapis.com/auth/devstorage.read_only","https://www.googleapis.com/auth/logging.write","https://www.googleapis.com/auth/monitoring","https://www.googleapis.com/auth/servicecontrol","https://www.googleapis.com/auth/service.management.readonly","https://www.googleapis.com/auth/trace.append" --spot --enable-autoscaling --total-min-nodes "0" --total-max-nodes "64" --location-policy "ANY" --enable-autoupgrade --enable-autorepair --max-surge-upgrade 0 --max-unavailable-upgrade 1
+# original commands suggested by Web Console:
+# gcloud beta container --project "camunda-researchanddevelopment" node-pools create "pool-c3-standard-8" --cluster "falko-benchmark" --zone "europe-west1-b" --node-version "1.27.3-gke.1700"    --machine-type "c3-standard-8" --image-type "COS_CONTAINERD" --disk-type "pd-ssd"             --disk-size "100" --metadata disable-legacy-endpoints=true --scopes "https://www.googleapis.com/auth/devstorage.read_only","https://www.googleapis.com/auth/logging.write","https://www.googleapis.com/auth/monitoring","https://www.googleapis.com/auth/servicecontrol","https://www.googleapis.com/auth/service.management.readonly","https://www.googleapis.com/auth/trace.append" --spot                 --enable-autoscaling --total-min-nodes "0" --total-max-nodes "64"  --location-policy "ANY" --enable-autoupgrade --enable-autorepair --max-surge-upgrade 0 --max-unavailable-upgrade 1
+# gcloud beta container --project "camunda-researchanddevelopment" node-pools create "pool-c4-standard-8" --cluster "falko-benchmark" --zone "europe-west1-b" --node-version "1.30.3-gke.1969001" --machine-type "c4-standard-8" --image-type "COS_CONTAINERD" --disk-type "hyperdisk-balanced" --disk-size "100" --metadata disable-legacy-endpoints=true --scopes "https://www.googleapis.com/auth/devstorage.read_only","https://www.googleapis.com/auth/logging.write","https://www.googleapis.com/auth/monitoring","https://www.googleapis.com/auth/servicecontrol","https://www.googleapis.com/auth/service.management.readonly","https://www.googleapis.com/auth/trace.append" --spot --num-nodes "1" --enable-autoscaling --min-nodes "0"       --max-nodes "256"       --location-policy "ANY" --enable-autoupgrade --enable-autorepair --max-surge-upgrade 1 --max-unavailable-upgrade 0
+# gcloud beta container --project "camunda-researchanddevelopment" node-pools create "pool-c4-standard-4" --cluster "falko-benchmark" --zone "europe-west1-b" --node-version "1.31.6-gke.1020000" --machine-type "c4-standard-4" --image-type "COS_CONTAINERD" --disk-type "hyperdisk-balanced" --disk-size "100" --metadata disable-legacy-endpoints=true --scopes "https://www.googleapis.com/auth/devstorage.read_only","https://www.googleapis.com/auth/logging.write","https://www.googleapis.com/auth/monitoring","https://www.googleapis.com/auth/servicecontrol","https://www.googleapis.com/auth/service.management.readonly","https://www.googleapis.com/auth/trace.append" --spot --num-nodes "0" --enable-autoscaling --total-min-nodes "0" --total-max-nodes "256" --location-policy "ANY" --enable-autoupgrade --enable-autorepair --max-surge-upgrade 1 --max-unavailable-upgrade 0 --shielded-integrity-monitoring --shielded-secure-boot
 
 .PHONY: kube-nodes-allocatable
 kube-nodes-allocatable: use-kube
@@ -64,7 +62,7 @@ clean-kube-gke: use-kube
 	gcloud container clusters list
 
 .PHONY: clean-node-pool
-clean-node-pool: use-kube
+clean-node-pool:
 	gcloud container node-pools delete "pool-$(machineType)" --cluster $(clusterName) --region $(region) --quiet
 
 .PHONY: use-kube
@@ -92,6 +90,6 @@ create-cloud-dns: fqdn
 
 .PHONY: delete-cloud-dns
 delete-cloud-dns: fqdn
-	gcloud dns record-sets delete $(fqdn) \
+	-gcloud dns record-sets delete $(fqdn) \
 	  --type=A \
 	  --zone=$(dnsManagedZone)
