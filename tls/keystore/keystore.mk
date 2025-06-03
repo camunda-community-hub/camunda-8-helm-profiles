1e1812293e7b83e047e608bd2f7781d4a576e924
Dave Paroulek <david.paroulek@camunda.com>
2025-06-03 07:41:53 -0700

    Add make targets to create k8s secret from keystore

--- tls/keystore/keystore.mk
+++ tls/keystore/keystore.mk
@@ -3,7 +3,7 @@ create-keystore: delete-keystore
 	openssl pkcs12 -export -in ./certs/$(certName)Server.crt -inkey ./certs/$(certName)Server.key \
                -out ./certs/$(certName)Server.p12 -name $(certName)-p12 \
                -CAfile ./certs/$(certName)CA.pem -caname $(certName)-ca
-	keytool -importkeystore -deststorepass camunda -destkeypass camunda -destkeystore ./certs/keystore.jks -srckeystore ./certs/$(certName)Server.p12 -srcstoretype PKCS12 -srcstorepass camunda
+	keytool -importkeystore -deststorepass camunda -destkeypass camunda -destkeystore ./certs/keystore.jks -srckeystore ./certs/$(certName)Server.p12 -srcstoretype PKCS12 -srcstorepass $(truststorePass)
 
 .PHONY: delete-keystore
 delete-keystore:
@@ -12,7 +12,7 @@ delete-keystore:
 
 .PHONY: list-keystore
 list-keystore:
-	keytool -list -v -keystore ./certs/keystore.jks -storepass camunda
+	keytool -list -v -keystore ./certs/keystore.jks -storepass $(truststorePass)
 
 .PHONY: create-truststore
 create-truststore: delete-truststore
@@ -24,4 +24,17 @@ delete-truststore:
 
 .PHONY: list-truststore
 list-truststore:
-	keytool -list -v -keystore ./certs/truststore.jks -storepass camunda
+	keytool -list -v -keystore ./certs/truststore.jks -storepass $(truststorePass)
+
+.PHONY: create-keystore-secret
+create-keystore-secret:
+	kubectl create secret generic camunda-keystore-secret --from-file=./certs/keystore.jks -n $(namespace)
+
+.PHONY: create-truststore-secret
+create-truststore-secret:
+	kubectl create secret generic camunda-truststore-secret --from-file=./certs/truststore.jks -n $(namespace)
+
+.PHONY: create-keycloak-secret
+create-keycloak-secret:
+	-kubectl -n $(namespace) delete secret "keycloak-secret"
+	kubectl -n $(namespace) create secret generic keycloak-secret --from-file=./certs/keystore.jks --from-file=./certs/truststore.jks
