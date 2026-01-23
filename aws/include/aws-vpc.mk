ee9f5c51384208ccd562d7d615e4d2f40d3b8ba1
Dave Paroulek <david.paroulek@camunda.com>
2026-01-23 17:45:05 -0500

    Manage aws subnets

--- aws/include/aws-vpc.mk
+++ aws/include/aws-vpc.mk
@@ -5,15 +5,21 @@ create-vpc:
       --region $(REGION) \
       --tag-specifications 'ResourceType=vpc,Tags=[{Key=Name,Value=$(VPC_NAME)}]'
 
-.PHONY: create-vpc-01
-create-vpc-01:
-	$(MAKE) -C $(root)/aws/vpc create-vpc VPC_NAME=$(DEPLOYMENT_NAME)-vpc-01 REGION=$(REGION) CIDR_BLOCK=$(CIDR_BLOCK)
-
-.PHONY: create-vpc-02
-create-vpc-02:
-	$(MAKE) -C $(root)/aws/vpc create-vpc VPC_NAME=$(DEPLOYMENT_NAME)-vpc-02 REGION=$(REGION) CIDR_BLOCK=$(CIDR_BLOCK)
-
 .PHONY: delete-vpc
 delete-vpc:
 	$(eval VPC_ID := $(shell aws ec2 describe-vpcs --filters "Name=tag:Name,Values=$(VPC_NAME)" --query "Vpcs[0].VpcId" --output text))
 	aws ec2 delete-vpc --vpc-id $(VPC_ID)
+
+.PHONY: create-subnet
+create-subnet:
+	$(eval VPC_ID := $(shell aws ec2 describe-vpcs --filters "Name=tag:Name,Values=$(VPC_NAME)" --query "Vpcs[0].VpcId" --output text))
+	aws ec2 create-subnet \
+	  --vpc-id $(VPC_ID) \
+	  --cidr-block $(SUBNET_CIDR_BLOCK) \
+	  --availability-zone $(AVAILABILITY_ZONE) \
+	  --tag-specifications 'ResourceType=subnet,Tags=[{Key=Name,Value=$(SUBNET_NAME)}]'
+
+.PHONY: delete-subnet
+delete-subnet:
+	$(eval SUBNET_ID := $(shell aws ec2 describe-subnets --filters "Name=tag:Name,Values=$(SUBNET_NAME)" --query "Subnets[0].SubnetId" --output text))
+	aws ec2 delete-subnet --subnet-id $(SUBNET_ID)
